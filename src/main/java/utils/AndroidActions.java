f70812cb8306b80f5ca34b31f3177280f9673a59
ThabangMonoane <tmonoane412@gmail.com>
2026-03-31 12:24:04 +0200

    Add attachScreenshot method to capture and log screenshots in tests

--- src/main/java/utils/AndroidActions.java
+++ src/main/java/utils/AndroidActions.java
@@ -1,5 +1,6 @@
 package utils;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
@@ -33,6 +34,7 @@ import java.util.*;
 import static org.openqa.selenium.interactions.PointerInput.Kind.TOUCH;
 import static org.openqa.selenium.interactions.PointerInput.MouseButton.LEFT;
 import static org.openqa.selenium.interactions.PointerInput.Origin.viewport;
+import static utils.Listeners.TEST_THREAD;
 
 public class AndroidActions extends AppiumUtils {
 
@@ -538,6 +540,25 @@ public class AndroidActions extends AppiumUtils {
         }
     }
 
+    public void attachScreenshot(AppiumDriver driver, String screenshotName) {
+        try {
+            ExtentTest test = getTest();
+            if (test != null && driver != null) {
+                String base64Screenshot = getBase64Screenshot(driver);
+                test.addScreenCaptureFromBase64String(base64Screenshot, screenshotName);
+            }
+        } catch (Exception e) {
+            log.warn("Could not capture screenshot: {0}", e.getMessage());
+            ExtentTest test = getTest();
+            if (test != null) {
+                test.warning("Could not capture screenshot: " + e.getMessage());
+            }
+        }
+    }
+
+     private ExtentTest getTest() {
+        return TEST_THREAD.get();
+    }
 
 
 
