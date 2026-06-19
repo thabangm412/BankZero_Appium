8692b45cdfd903cc0dcc7c5dadb7c706b682402e
ThabangMonoane <tmonoane412@gmail.com>
2026-06-19 13:53:27 +0200

    Fix account statement retrieval for 7, 32, and 45 days notice tests

--- src/test/java/tests/NewAccounts/ThirtyTwoDaysNotice/StatementsAndLettersFrom32DaysNoticeTests.java
+++ src/test/java/tests/NewAccounts/ThirtyTwoDaysNotice/StatementsAndLettersFrom32DaysNoticeTests.java
@@ -51,7 +51,7 @@ public class StatementsAndLettersFrom32DaysNoticeTests extends BaseTestsConfig {
 
         accountMenuActions.clickAccountMenuActionsOption("32 Days Notice");
         statementsAndLettersPage.clickAccountStatements();
-        statementsAndLettersPage.getAccountStatements(2);
+        statementsAndLettersPage.getAccountStatements(1);
         statementsAndLettersPage.clickEmailButton();
         Assert.assertEquals(statementsAndLettersPage.getDocumentRequestStatus(),"Email sent successful. Please check your inbox");
         attachScreenshot(DriverManager.driver,"Account Statements Email Sent");
