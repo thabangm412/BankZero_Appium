1e623dfd761f6d4597543f505df4c7e3fe2906f7
ThabangMonoane <tmonoane412@gmail.com>
2026-09-25 08:48:22 +0200

    Update BusinessDataFactory with new business registration data and add Owners and Officials data model

--- src/test/java/factory/BusinessDataFactory.java
+++ src/test/java/factory/BusinessDataFactory.java
@@ -6,22 +6,22 @@ import models.OwnersAndOfficials;
 import models.User;
 
 public class BusinessDataFactory {
-    public static BusinessRegData validBusinessRegData() {
+    public static BusinessRegData  validBusinessRegData() {
         return BusinessRegData.builder()
                 .user(new User("Reo", "11111"))
                 .businessType("Pty (Ltd)")
-                .tradingName("Business Test")
+                .tradingName("Reo Business Test")
                 .registeredName("Business (Pty) Ltd")
-                .registrationNo("2026/160106/07")
+                .registrationNo("2026/160103/07")
                 .sicGroup("Construction")
                 .sicIndustry("Civil engineering")
-                .notifyEmail("testBusiness01@bankzero.co.za")
+                .notifyEmail("testBusiness03@bankzero.co.za")
                 .street("4 Georgian Crescent West")
                 .city("Bryanston, Sandton")
                 .postalCode("2191")
                 .successMsg("Account and card successfully added. You can access this account from your canvas.")
                 .cardPin("11111")
-                .fundsAccount("Business")
+                .fundsAccount("Reo")
                 .build();
     }
 
@@ -63,6 +63,24 @@ public class BusinessDataFactory {
                 .levelA_amount("1000")
                 .levelB_amount("500")
                 .levelC_amount("100")
+                .build();
+    }
+
+    public static OwnersAndOfficials businessOwnersAndOfficials(){
+        return OwnersAndOfficials.builder()
+                .user(new User("Isagi", "33333"))
+                .role1("Authoriser")
+                .role2("Beneficial owner")
+                .role3("Director")
+                .role4("Mandated Official")
+                .role5("Shareholder")
+                .ownerName("Last Comms Regression")
+                .nationality("South Africa")
+                .cellNumber("0676336783")
+                .levelA_amount("1000")
+                .levelB_amount("500")
+                .levelC_amount("100")
+                .registrationNo("2024/6985447/07")
                 .build();
     }
 
